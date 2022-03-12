d31f328d8a972f35798afa82cc73477079e8be75
xx <15833077492@163.com>
2022-03-12 19:49:11 +0800

    修改部分关键字驱动

--- basic/keyword.py
+++ basic/keyword.py
@@ -2,15 +2,15 @@
 from time import sleep as sleep_
 
 from selenium import webdriver
-from selenium.webdriver import ActionChains
+from selenium.webdriver import ActionChains, Keys
 from selenium.webdriver.support.wait import WebDriverWait
 
 
 class WebKey:
     # 打开浏览器
-    def browser(self, driver):
-        self.driver = webdriver.Chrome()
-        # self.driver = driver
+    def __init__(self, driver):
+        # self.driver = webdriver.Chrome()
+        self.driver = driver
 
     # 打开URL网址
     def url(self, url):
@@ -22,6 +22,7 @@ class WebKey:
 
     # 定位到指定元素，并使用红色的框圈住定位到的元素
     def location_list(self, element_list):
+        self.scroll_to(element_list[0], element_list[1])
         self._locator_station(self.location(element_list[0], element_list[1]))
         return self.location(element_list[0], element_list[1])
 
@@ -33,6 +34,12 @@ class WebKey:
     def input(self, element_list, value):
         self.location_list(element_list).send_keys(value)
 
+    # 修改文本框数据
+    def input_modify(self, element_list, value):
+        el = self.location_list(element_list)
+        el.clear()
+        el.send_keys(value)
+
     # 鼠标悬浮
     def hover(self, element_list):
         ActionChains(self.driver).move_to_element(self.location_list(element_list)).perform()
@@ -42,7 +49,7 @@ class WebKey:
         self.driver.execute_script(
             "arguments[0].setAttribute('style', arguments[1]);",
             element,
-            "border: 3px solid red;"  # 边框border:2px; red红色
+            "border: 3px solid blue;"  # 边框border:2px; red红色
         )
 
     # 强制等待
@@ -51,7 +58,8 @@ class WebKey:
 
     # 切换句柄
     def switch_handle(self, value):
-        self.driver.switch_to.window(value)
+        handles = self.driver.window_handles
+        self.driver.switch_to.window(handles[value])
 
     # 切换iframe
     def iframe(self, element_list):
@@ -69,11 +77,30 @@ class WebKey:
     def quit(self):
         self.driver.quit()
 
+    # 使浏览器页面到达指定元素
+    def scroll_to(self, key, value):
+        js = 'arguments[0].scrollIntoView()'
+        self.driver.execute_script(js, self.location(key, value))
+
     # 显示等待
     def show_wait(self, element_list):
         WebDriverWait(self.driver, 10, 0.5).until(
             lambda el: self.location_list(element_list), "元素未找到")
 
-    def A(self,filename):
-        # self.driver.save_screenshot(filename)
-        self.driver.get_screenshot_as_file(filename)
+    # 截图
+    def screenshot(self, filename):
+        self.driver.save_screenshot(filename)
+
+    # 通过获取文本内容进行断言
+    def assert_text(self, element_list, text):
+        if self.location_list(element_list).text == text:
+            return True
+        else:
+            return False
+
+    # 通过判断元素是否存在进行断言
+    def assert_exist(self, element_list):
+        if self.location_list(element_list):
+            return True
+        else:
+            return False
